3d59b2068b513786de81cde0a2865678d4e66b61
conr2286 <conr2286@gmail.com>
2023-05-03 22:10:20 -0700

    Add -x option to vfo
    -x disables vfo clk0

--- tools/vfo/vfo.c
+++ tools/vfo/vfo.c
@@ -1,7 +1,7 @@
 /*
  * vfo --- Set the VFO CLK0 to the specified frequency
  *
- * Usage:  vfo <frequency>
+ * Usage:  vfo <[-D][-x] frequency>
  *
  * 	frequency	VFO operating frequency in HZ
  *
@@ -26,18 +26,21 @@ int main(int argc, char *argv[]) {
 	int c;
 
 	uint32_t freqHz = 10000000;	//VFO frequency in HZ
-
-	DK_SET_VAR(vfo,0);
+	bool halt=false;
 
 	opterr = 0;
 
-	while ((c = getopt(argc, argv, "D")) != -1) {
+	while ((c = getopt(argc, argv, "Dx")) != -1) {
 		switch (c) {
 		case 'D':
 			DK_SET_VAR(vfo,1);
 			DK_SET_VAR(si5351,1);
 			DK_SET_VAR(i2cb,1);
 			DK_SET_VAR(i2c,1);
+			break;
+		case 'x':
+			DK_TRACE(vfo,"Got -x\n");
+			halt = true;
 			break;
 		case '?':
 			fprintf(stderr, "Usage:  vfo [-D] <frequency>\n");
@@ -47,23 +50,24 @@ int main(int argc, char *argv[]) {
 		} //switch
 	} //while
 
-	//Check for missing argument
-	if (optind>=argc) {
-		fprintf(stderr, "Usage:  vfo [-D] <frequency>\n");
-		return 1;
-	}
-
-	//Parse requested frequency
-	DK_TRACE(vfo,"arg[%d]=%s\n",optind,argv[optind]);
-	sscanf(argv[optind],"%u",&freqHz);
-
-
 	//Initialize the SI5351a
 	int err = si5351Init("/dev/i2c-1", 0x60, 25000000, 10);
 	if (err < 0) {
 		fprintf(stderr,"si5351Init failed\n");
 		exit(-1);
 	}
+
+	//Halt the VFO?
+	if (halt) {
+		DK_TRACE(vfo,"Halting\n");
+		si5351ClockEnable(0,false);			//Disable Clk0
+		return 0;
+	}
+
+	//Parse requested frequency
+	DK_TRACE(vfo,"arg[%d]=%s\n",optind,argv[optind]);
+	sscanf(argv[optind],"%u",&freqHz);
+
 
 	//Start the Clk0 oscillator at requested frequency
 	si5351SetFrequency(0, freqHz, 0);
